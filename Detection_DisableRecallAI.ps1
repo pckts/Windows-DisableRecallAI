ffb29a8a324df3b4d4b9eee45a165ba12974a50a
Anders <atl@pck.dk>
2024-07-31 13:50:09 +0200

    Add files via upload

---
+++ Detection_DisableRecallAI.ps1
@@ -0,0 +1,3 @@
+﻿$RecallState = Get-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WindowsAI\
+if ($RecallState.DisableAIDataAnalysis -eq "1")
+{exit 0} else {exit 1}
---
+++ Remediation_DisableRecallAI.ps1
@@ -0,0 +1,5 @@
+﻿$KeyExist = Test-Path -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsAI\"
+if ($KeyExist -ne $true)
+{New-item -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsAI\"}
+Remove-itemproperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsAI\" -Name 'DisableAIDataAnalysis'
+New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsAI\" -Name 'DisableAIDataAnalysis' -Value 1 -PropertyType DWord  
